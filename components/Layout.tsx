356c4f2ba65b7232d12e4cd4c6b2fdb043d2aff4
Uri_Berger <uribe1927@gmail.com>
2020-07-01 02:12:36 +0300

    first commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
             <title>{title}</title>
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <link rel="icon" href="/favicon.ico" />
+            {/*<link rel="icon" href="/favicon.ico" />*/}
         </Head>
 
         {children}
