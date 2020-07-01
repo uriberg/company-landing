5c2d3e193c9bc7a1a040a054be6ce1ee4dc9947a
Uri_Berger <uribe1927@gmail.com>
2020-07-01 17:55:36 +0300

    *minor margin and padding tweaks

--- components/about.tsx
+++ components/about.tsx
@@ -10,12 +10,14 @@ const About = () => (
     <div className="about">
         <div className="about__url">{about.url}</div>
         <div className="about__headline">{about.headline}</div>
+        <div>
         {about.paragraphs.map((item: any, index) => {
             return <div className="about__par" key={index}>
                          <h2 className="about__par--headline">{item.headline}</h2>
                          <div className="about__par--text">{item.text}</div>
                     </div>
         })}
+        </div>
     </div>
 );
 
